f97d4d9378eba84ec2a6c7cbdd3aeeb000ea12f8
bhavana <bhavana.vayshali@gmail.com>
2023-11-28 22:32:27 +0530

    static website  for hotel

--- inmakesapp/urls.py
+++ inmakesapp/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('',views.index,name= 'index')
     
-    path('',views.home ,name = 'home'),
-    path('result',views.result ,name = 'result')
+    
 ]
--- inmakesapp/views.py
+++ inmakesapp/views.py
@@ -3,15 +3,6 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def home(request):
-    return render(request,'home.html')
-def result(request):
-    x = int(request.GET['num1'])
-    y= int(request.GET['num2'])
-    add = x+y
-    mul = x*y
-    sub = x-y
-    div = x/y
-    return render(request,'result.html',{'add':add,'mul':mul,'sub':sub,'div':div})
-
+def index(request):
+    return render(request,'index.html')
 
